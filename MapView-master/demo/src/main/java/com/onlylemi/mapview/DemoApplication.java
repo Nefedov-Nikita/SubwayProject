be85f1ac2a6b1bba529095e4ad88912c216b44fb
Privet-Medvet <60381178+Privet-Medvet@users.noreply.github.com>
2020-05-27 19:49:41 +0300

    Demo
    Demo

--- MapView-master/demo/src/main/java/com/onlylemi/mapview/DemoApplication.java
+++ MapView-master/demo/src/main/java/com/onlylemi/mapview/DemoApplication.java
@@ -6,12 +6,6 @@ import android.content.Context;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
-/**
- * DemoApplication
- *
- * @author: onlylemi
- * @time: 2016-05-14 12:07
- */
 public class DemoApplication extends Application {
 
     private static Context context;
