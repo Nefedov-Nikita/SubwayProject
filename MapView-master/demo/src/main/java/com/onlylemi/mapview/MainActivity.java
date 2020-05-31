7efa83747e2ebf8686db072719c673066d815da7
Privet-Medvet <60381178+Privet-Medvet@users.noreply.github.com>
2020-05-31 20:15:20 +0300

    Splash Screen logo
    Устранена ошибка логотипа

--- MapView-master/demo/src/main/java/com/onlylemi/mapview/MainActivity.java
+++ MapView-master/demo/src/main/java/com/onlylemi/mapview/MainActivity.java
@@ -9,22 +9,26 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView logo;
+
     //private static final String TAG = "MainActivity";
-    private static int SplashTime = 3000;
+    private static int SplashTime = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        logo = (ImageView) findViewById(R.id.imageView);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(MainActivity.this, MarkLayerTestActivity
                         .class));
+                logo.setImageDrawable(null);
             }
         },SplashTime);
     }
